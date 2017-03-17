4dd3e23250a4f73a664fb3bfc23d8e997561e7f3
Christian Zufferey <christian@zufferey.com>
2017-03-17 16:54:49 +0100

    - amélioré totalement mon Helloworld afin de pouvoir tester le remote debugger

--- toto.js
+++ toto.js
@@ -1,10 +1,32 @@
+//nodejs-hello-world
+//Petit Helloworld en nodejs pour tester le remote debugger
+//zf170317.1647
+//MIT License
+//Copyright (c) 2017 christian@zufferey.com
+
+
 var express = require('express')
+var titi = require("./titi.js")
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World! yeah cela marche 1704')
+
+  text = ""
+  result = titi.tata(33, 3)
+  text = "<p>Hello World! <br> ça marche en remote 1648.<p>"
+
+  if (result < 18) {
+    text += ('<p>Le résultat est: ' + result + "</p>")
+  }
+
+  //  debugger;
+
+  res.send(text)
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+
+//ssh -D 3333 ubuntu@zuzu123.node.consul
