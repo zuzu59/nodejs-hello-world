7bd2c06e5c60b3074717ef60ebb696fae7ab6a0c
Christian Zufferey <christian@zufferey.com>
2017-03-17 18:13:43 +0100

    - ajouté la possibilité de service aussi un simple fichier HTML (toto.html) avec le petit serveur Express

--- toto.js
+++ toto.js
@@ -1,31 +1,37 @@
 //nodejs-hello-world
 //Petit Helloworld en nodejs pour tester le remote debugger
-//zf170317.1647
+//zf170317.1744
 //MIT License
 //Copyright (c) 2017 christian@zufferey.com
 
-
 var express = require('express')
-var titi = require("./titi.js")
 var app = express()
+var path = require('path');
+var titi = require("./titi.js")
+var tutu = require("./tutu.js")
 
-app.get('/', function (req, res) {
+//route en dynamique
+app.get('/', function(req, res) {
+    text = ""
+    result = titi.tata(33, 3)
+    text = "<p>Hello World! <br> ça marche en remote 1656.<p>"
 
-  text = ""
-  result = titi.tata(33, 3)
-  text = "<p>Hello World! <br> ça marche en remote 1648.<p>"
+    if (result < 18) {
+        text += ('<p>Le résultat est: ' + result + "</p>")
+    }
 
-  if (result < 18) {
-    text += ('<p>Le résultat est: ' + result + "</p>")
-  }
+    //  debugger;
 
-  //  debugger;
-
-  res.send(text)
+    res.send(text)
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+//route une fichier HTML statique
+app.get('/toto.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/toto.html'));
+})
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!')
 })
 
 
